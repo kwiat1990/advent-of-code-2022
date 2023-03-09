0e36d32fa97cc6ed8a6211039216052db898c1df
Mateusz Kwiatkowski <kwiat1990@gmail.com>
2023-03-09 16:22:30 +0100

    Refactor solutions for day 8

--- src/bin/08.rs
+++ src/bin/08.rs
@@ -1,110 +1,75 @@
-fn create_grid(input: &str) -> (Vec<Vec<u32>>, Vec<Vec<u32>>) {
-    let mut columns: Vec<Vec<u32>> = vec![];
-    let mut rows: Vec<Vec<u32>> = vec![];
+use std::ops::Range;
 
-    for line in input.lines() {
-        let chars: Vec<u32> = line.chars().map(|x| x.to_digit(10).unwrap()).collect();
-        rows.push(chars.clone());
-        for (i, item) in chars.iter().enumerate() {
-            match columns.get_mut(i) {
-                Some(x) => x.push(*item),
-                None => columns.push(vec![*item]),
-            }
-        }
-    }
-
-    (columns, rows)
+fn create_grid(input: &str) -> Vec<Vec<u32>> {
+    input
+        .lines()
+        .map(|line| line.chars().map(|ch| ch.to_digit(10).unwrap()).collect())
+        .collect()
 }
 
-fn count_edges(grid: (&Vec<Vec<u32>>, &Vec<Vec<u32>>)) -> usize {
-    let columns = (grid.0.len() - 2) * 2; // remove item from top and bottom corners (-2)
-    let rows = grid.1.len() * 2;
-    columns + rows
+fn get_from_directions(grid: &[Vec<u32>], x: usize, y: usize) -> [Vec<u32>; 4] {
+    let row = &grid[y];
+    let column: Vec<u32> = grid.iter().map(|row| row[x]).collect();
+
+    let (left, right) = row.split_at(x);
+    let (top, bottom) = column.split_at(y);
+
+    // seen from perspective of the current element, that's why reversed
+    let left: Vec<u32> = left.iter().copied().rev().collect();
+    let top: Vec<u32> = top.iter().copied().rev().collect();
+    // without the current element itself
+    let right = right[1..].to_vec();
+    let bottom = bottom[1..].to_vec();
+
+    [left, right, top, bottom]
 }
 
+fn cartesian_product(range: Range<usize>) -> Vec<(usize, usize)> {
+    range
+        .clone()
+        .flat_map(|y| range.clone().map(move |x| (x, y)))
+        .collect()
+}
+
+// After refactoring both solutions became >7x slower but the code is much more readable and concise.
 pub fn part_one(input: &str) -> Option<usize> {
-    let (columns, rows) = create_grid(input);
-    let mut visible_items = count_edges((&columns, &rows));
+    let grid = create_grid(input);
+    let edges_count = (grid.len() - 1) * 4;
 
-    let interior_rows = &rows[1..rows.len() - 1];
+    let interior_count = cartesian_product(1..grid.len() - 1)
+        .iter()
+        .map(|&(x, y)| {
+            let height = grid[x][y];
+            get_from_directions(&grid, y, x)
+                .iter()
+                .map(|direction| direction.iter().all(|h| *h < height))
+                .any(|visible| visible)
+        })
+        .filter(|visible| *visible)
+        .count();
 
-    for (i, row) in interior_rows.iter().enumerate() {
-        for (y, item) in row.iter().enumerate() {
-            if y == 0 || y == row.len() - 1 {
-                continue;
-            }
-
-            let has_bigger = |slice: &[u32]| -> bool { slice.iter().any(|x| x >= item) };
-
-            let invisible_in_row = has_bigger(&row[..y]) && has_bigger(&row[y + 1..]);
-            let invisible_in_column =
-                has_bigger(&columns[y][..=i]) && has_bigger(&columns[y][i + 2..]);
-
-            if !invisible_in_row || !invisible_in_column {
-                visible_items += 1;
-            }
-        }
-    }
-
-    Some(visible_items)
+    Some(edges_count + interior_count)
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    let (columns, rows) = create_grid(input);
-    let interior_rows = &rows[1..rows.len() - 1];
-    let mut highest_score = 0;
+pub fn part_two(input: &str) -> Option<usize> {
+    let grid = create_grid(input);
 
-    for (i, row) in interior_rows.iter().enumerate() {
-        for (y, item) in row.iter().enumerate() {
-            let mut count = vec![0, 0, 0, 0];
-
-            let row_start = row[..y].iter().rev();
-            let row_end = row[y + 1..].iter();
-            let col_start = columns[y][..=i].iter().rev();
-            let col_end = columns[y][i + 2..].iter();
-
-            for x in row_start {
-                if x < item {
-                    count[0] += 1;
-                } else {
-                    count[0] += 1;
-                    break;
-                }
-            }
-            for x in row_end {
-                if x < item {
-                    count[1] += 1;
-                } else {
-                    count[1] += 1;
-                    break;
-                }
-            }
-
-            for x in col_start {
-                if x < item {
-                    count[2] += 1;
-                } else {
-                    count[2] += 1;
-                    break;
-                }
-            }
-            for x in col_end {
-                if x < item {
-                    count[3] += 1;
-                } else {
-                    count[3] += 1;
-                    break;
-                }
-            }
-
-            let current_score = count.iter().product();
-            if current_score > highest_score {
-                highest_score = current_score;
-            }
-        }
-    }
-
-    Some(highest_score)
+    cartesian_product(1..grid.len() - 1)
+        .iter()
+        .map(|&(x, y)| {
+            let height = grid[x][y];
+            get_from_directions(&grid, y, x)
+                .iter()
+                .map(|direction| {
+                    direction
+                        .iter()
+                        .position(|h| *h >= height)
+                        .map(|count| count + 1)
+                        .unwrap_or_else(|| direction.len())
+                })
+                .product()
+        })
+        .max()
 }
 
 fn main() {
