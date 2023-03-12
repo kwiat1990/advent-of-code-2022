273e83fc4de2144ae20da17c6fe29fdb354eda91
Mateusz Kwiatkowski <kwiat1990@gmail.com>
2023-03-12 13:04:15 +0100

    Refactor solution for day 8 - 1d vector

--- src/bin/08.rs
+++ src/bin/08.rs
@@ -1,46 +1,71 @@
 use std::ops::Range;
 
-fn create_grid(input: &str) -> Vec<Vec<u32>> {
-    input
-        .lines()
-        .map(|line| line.chars().map(|ch| ch.to_digit(10).unwrap()).collect())
-        .collect()
+struct Grid {
+    data: Vec<u32>,
+    size: usize,
 }
 
-fn get_from_directions(grid: &[Vec<u32>], x: usize, y: usize) -> [Vec<u32>; 4] {
-    let row = &grid[y];
-    let column: Vec<u32> = grid.iter().map(|row| row[x]).collect();
+impl Grid {
+    fn get_cell(&self, col: usize, row: usize) -> u32 {
+        self.data[col + self.size * row]
+    }
 
-    let (left, right) = row.split_at(x);
-    let (top, bottom) = column.split_at(y);
+    fn get_row(&self, row: usize) -> Vec<u32> {
+        self.data[(row * self.size)..((row + 1) * self.size)].to_vec()
+    }
 
-    // seen from perspective of the current element, that's why reversed
-    let left: Vec<u32> = left.iter().copied().rev().collect();
-    let top: Vec<u32> = top.iter().copied().rev().collect();
-    // without the current element itself
-    let right = right[1..].to_vec();
-    let bottom = bottom[1..].to_vec();
+    fn get_col(&self, col: usize) -> Vec<u32> {
+        (0..self.size)
+            .map(|i| self.data[i * self.size + col])
+            .collect()
+    }
 
-    [left, right, top, bottom]
+    fn get_from_directions(&self, col: usize, row: usize) -> [Vec<u32>; 4] {
+        let full_row = self.get_row(row);
+        let full_col = self.get_col(col);
+
+        let (left, right) = full_row.split_at(col);
+        let (top, bottom) = full_col.split_at(row);
+
+        // seen from perspective of the current element, that's why reversed
+        let left: Vec<u32> = left.iter().copied().rev().collect();
+        let top: Vec<u32> = top.iter().copied().rev().collect();
+        // without the current element itself
+        let right = right[1..].to_vec();
+        let bottom = bottom[1..].to_vec();
+
+        [left, right, top, bottom]
+    }
+}
+
+fn create_grid(input: &str) -> Vec<u32> {
+    input
+        .lines()
+        .flat_map(|line| line.chars().map(|ch| ch.to_digit(10).unwrap()))
+        .collect()
 }
 
 fn cartesian_product(range: Range<usize>) -> Vec<(usize, usize)> {
     range
         .clone()
-        .flat_map(|y| range.clone().map(move |x| (x, y)))
+        .flat_map(|row| range.clone().map(move |col| (col, row)))
         .collect()
 }
 
 // After refactoring both solutions became >7x slower but the code is much more readable and concise.
 pub fn part_one(input: &str) -> Option<usize> {
-    let grid = create_grid(input);
-    let edges_count = (grid.len() - 1) * 4;
+    let grid = Grid {
+        size: input.lines().next().unwrap().len(),
+        data: create_grid(input),
+    };
 
-    let interior_count = cartesian_product(1..grid.len() - 1)
-        .iter()
-        .map(|&(x, y)| {
-            let height = grid[x][y];
-            get_from_directions(&grid, y, x)
+    let edges_count = (grid.size - 1) * 4;
+
+    let interior_count = cartesian_product(1..grid.size - 1)
+        .into_iter()
+        .map(|(col, row)| {
+            let height = grid.get_cell(col, row);
+            grid.get_from_directions(col, row)
                 .iter()
                 .map(|direction| direction.iter().all(|h| *h < height))
                 .any(|visible| visible)
@@ -52,13 +77,16 @@ pub fn part_one(input: &str) -> Option<usize> {
 }
 
 pub fn part_two(input: &str) -> Option<usize> {
-    let grid = create_grid(input);
+    let grid = Grid {
+        size: input.lines().next().unwrap().len(),
+        data: create_grid(input),
+    };
 
-    cartesian_product(1..grid.len() - 1)
-        .iter()
-        .map(|&(x, y)| {
-            let height = grid[x][y];
-            get_from_directions(&grid, y, x)
+    cartesian_product(1..grid.size - 1)
+        .into_iter()
+        .map(|(col, row)| {
+            let height = grid.get_cell(col, row);
+            grid.get_from_directions(col, row)
                 .iter()
                 .map(|direction| {
                     direction
